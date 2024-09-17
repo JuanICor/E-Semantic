5f64f21350f683f87bda744b4c51059fef739425
JuanICor <juan.cortez@mi.unc.edu.ar>
2024-09-17 16:58:53 -0300

    The parser works with the samples 'op_mult_sample01.c' and 'op_mult_sample02.c'

--- Classes/tree_parser.py
+++ Classes/tree_parser.py
@@ -1,5 +1,5 @@
 import tree_sitter_c as CLanguage
-from tree_sitter import Language, Parser, Tree, Query, Node, TreeCursor
+from tree_sitter import *
 
 from typing import Final
 
@@ -19,30 +19,39 @@ class TreeParser():
     def get_matches(self, query: str) -> dict[str, list[Node]]:
         q : Query = self.__language.query(query)
         
-        return query.captures(self.__tree.root_node)
+        return q.captures(self.__tree.root_node)
 
-    def get_nodes_between(self, node1: Node, node2: Node):
+    def __goto_parent__(self, node: Node) -> TreeCursor:
+        cursor: TreeCursor = self.__tree.root_node.walk()
+
+        while cursor.goto_first_child_for_byte(node.start_byte):
+            pass
+        
+        cursor.goto_parent()
+
+        return cursor
+    
+    def get_nodes_between(self, node1: Node, node2: Node) -> list[Node]:
         sbyte: int = node1.end_byte
-        ebyte: int = node2.start_byte
+        ebyte: int = node2.start_byte  
 
-        traveler: TreeCursor = self.__tree.root_node.walk()
-        traveler.goto_first_child_for_byte(sbyte)
+        traveler: TreeCursor = self.__goto_parent__(node1)
 
         nodes_between: list[Node] = []
 
-        def is_in_between(target: Node, start_pos: int, end_pos: int) -> bool:
-            return target.start_byte > start_pos and target.end_byte < end_pos
+        def is_in_between(target: Node) -> bool:
+            return target.start_byte > sbyte and target.end_byte < ebyte
 
         if traveler.goto_first_child():
             curr_node: Node = traveler.node
 
-            if is_in_between(curr_node, sbyte, ebyte):
+            if is_in_between(curr_node):
                 nodes_between.append(curr_node)
 
             while traveler.goto_next_sibling():
-                curr_node: Node = traveler.node
+                curr_node = traveler.node
 
-                if is_in_between(curr_node, sbyte, ebyte):
+                if is_in_between(curr_node):
                     nodes_between.append(curr_node)
-        
+
         return nodes_between
--- main.py
+++ main.py
@@ -1,11 +1,82 @@
-from Classes.arithmetics_graph import *
 from Classes.tree_parser import TreeParser
+from egraph import *
+from tree_sitter import Node
 
-def get_code_between_comments(parser: TreeParser) -> str:
+from typing import Final, Callable
+
+PATH: Final = "./Samples/eqClass"
+
+operations: dict[str, Callable[[Arithmetics, Arithmetics], Arithmetics]] = {'+' : sum, '*': mult, '<<': lshift}
+
+def get_code_between_comments(parser: TreeParser) -> list[Node]:
     query: str = '''
                 (comment) @comments
                 '''
 
     matches: dict[str, list[Node]] = parser.get_matches(query)
-    nodes = matches['comments']
+    nodes: list[Node] = matches['comments']
     
+    return parser.get_nodes_between(nodes[0], nodes[1])
+
+def expression_construct(n: Node) -> Arithmetics:
+    nodeType : str = n.type
+
+    if nodeType == "identifier":
+        return Arithmetics.var(n.text.decode("utf8"))
+
+    elif nodeType == "number_literal":
+        return Arithmetics(int(n.text))
+
+    elif nodeType == "binary_expression":
+        operation: Callable[[Arithmetics, Arithmetics], Arithmetics] = operations.get(n.child(1).type)
+        return operation(expression_construct(n.child(0)),
+                         expression_construct(n.child(2))
+                        )
+
+    elif nodeType == "assignment_expression":
+        operators: set[str] = operations.keys()
+        operation: Callable[[Arithmetics, Arithmetics], Arithmetics] = None
+
+        for op in operators:
+            if op in n.child(1).type:
+                operation = operations.get(op)
+                break
+
+        temp_node: Node = n.child(0)
+
+        if operation is not None:
+            return assign(Arithmetics.var(temp_node.text.decode("utf8")),
+                              operation(expression_construct(n.child(0)),
+                                        expression_construct(n.child(2))
+                                       )
+                         )
+        else:
+            return assign(Arithmetics.var(temp_node.text.decode("utf8")),
+                              expression_construct(n.child(2))
+                         )
+    
+    pass
+
+if __name__ == "__main__":
+
+    sample_nums: list[str] = ["00"]
+    file1: Final = "op_mult_sample01.c"
+    file2: Final = "op_mult_sample02.c"
+
+    sample01: TreeParser = TreeParser(PATH + sample_nums[0] + "/" + file1)
+    sample02: TreeParser = TreeParser(PATH + sample_nums[0] + "/" + file2)
+
+    target_1: Node = get_code_between_comments(sample01)[0]
+    target_2: Node = get_code_between_comments(sample02)[0]
+
+    expr1 = egraph.let("expr1", expression_construct(target_1.child(0)))
+    expr2 = egraph.let("expr2", expression_construct(target_2.child(0)))
+
+    egraph.run(10)
+
+
+    try:
+        egraph.check(eq(expr1).to(expr2))
+        print("The Expressions are equal.")
+    except Exception:
+        print("The Expression are not equal.")
